7f80fcf62c5013cb2df62fcc5e973ada50d84b52
hrg <hrgupta@outlook.com>
2020-03-27 19:48:15 -0500

    add bhagavadgita spider

---
+++ scriptures/spiders/bhagavadgita_spider.py
@@ -0,0 +1,42 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+# This class is the main spider class. It will crawl the main website.
+
+class MantraSpider(scrapy.Spider):
+    name = "mantraspider"
+
+    def start_requests(self):
+        start_urls = [
+            'https://www.gitasupersite.iitk.ac.in/srimad',
+        ]
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse_mantra)
+
+    # Function to get all the mantras from the website
+    def parse_mantra(self, response):
+
+        for mantra in response.css('div.views-field.views-field-body'):
+            shloka = mantra.css('font::text').getall()
+        
+        shloka = list(map(lambda x:x.replace("\n",""),shloka)) # remove new-line chars from string
+
+        shloka = list(filter(lambda x:x != "",shloka)) # remove empty strings
+
+        yield {
+            'title': response.css('h1.page-title::text').get(),
+            'mantra': shloka
+        }
+
+        first_page = response.css('p.navigation_block a::attr(href)')[0].get()
+        next_page = response.css('p.navigation_block a::attr(href)')[2].get()
+        if next_page != first_page:
+            yield response.follow(next_page, callback=self.parse_mantra)
+
+
+process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'csv',
+    'FEED_URI': '../../data/raw/gitas/bhagavadgita.csv'
+})
+process.crawl(MantraSpider)
+process.start()
