a887763e7a8f2db40da7d80764ab32143a9afb88
hrg <hrgupta@outlook.com>
2021-02-23 01:24:20 -0600

    formated with black

--- scriptures/spiders/bhagavadgita_spider.py
+++ scriptures/spiders/bhagavadgita_spider.py
@@ -9,7 +9,7 @@ class MantraSpider(scrapy.Spider):
 
     def start_requests(self):
         start_urls = [
-            'https://www.gitasupersite.iitk.ac.in/srimad',
+            "https://www.gitasupersite.iitk.ac.in/srimad",
         ]
         for url in start_urls:
             yield scrapy.Request(url=url, callback=self.parse_mantra)
@@ -17,8 +17,8 @@ class MantraSpider(scrapy.Spider):
     # Function to get all the mantras from the website
     def parse_mantra(self, response):
 
-        for mantra in response.css('div.views-field.views-field-body'):
-            shloka = mantra.css('font::text').getall()
+        for mantra in response.css("div.views-field.views-field-body"):
+            shloka = mantra.css("font::text").getall()
 
         # remove new-line chars from string
         shloka = list(map(lambda x: x.replace("\n", ""), shloka))
@@ -26,20 +26,16 @@ class MantraSpider(scrapy.Spider):
         # remove empty strings
         shloka = list(filter(lambda x: x != "", shloka))
 
-        yield {
-            'title': response.css('h1.page-title::text').get(),
-            'mantra': shloka
-        }
+        yield {"title": response.css("h1.page-title::text").get(), "mantra": shloka}
 
-        first_page = response.css('p.navigation_block a::attr(href)')[0].get()
-        next_page = response.css('p.navigation_block a::attr(href)')[2].get()
+        first_page = response.css("p.navigation_block a::attr(href)")[0].get()
+        next_page = response.css("p.navigation_block a::attr(href)")[2].get()
         if next_page != first_page:
             yield response.follow(next_page, callback=self.parse_mantra)
 
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'csv',
-    'FEED_URI': '../../data/raw/gitas/bhagavadgita.csv'
-})
+process = CrawlerProcess(
+    settings={"FEED_FORMAT": "csv", "FEED_URI": "../../data/raw/gitas/bhagavadgita.csv"}
+)
 process.crawl(MantraSpider)
 process.start()
--- scriptures/spiders/gitas_spider.py
+++ scriptures/spiders/gitas_spider.py
@@ -1,5 +1,5 @@
-import scrapy
 import re
+import scrapy
 from scrapy.crawler import CrawlerProcess
 
 # This class is the main spider class. It will crawl the main website.
@@ -7,41 +7,36 @@ from scrapy.crawler import CrawlerProcess
 
 class MantraSpider(scrapy.Spider):
     name = "mantraspider"
-    start_urls = ['https://www.gitasupersite.iitk.ac.in/minigita/texts']
+    start_urls = ["https://www.gitasupersite.iitk.ac.in/minigita/texts"]
 
     def parse(self, response):
 
-        links = response.css('div.field-item.even a::attr(href)').getall()
+        links = response.css("div.field-item.even a::attr(href)").getall()
         for url in links:
             yield response.follow(url, callback=self.parse_mantra)
 
     # Function to get all the mantras from the website
     def parse_mantra(self, response):
 
-        for mantra in response.css('div.views-field.views-field-body'):
-            shloka = mantra.css('font::text').getall()
+        for mantra in response.css("div.views-field.views-field-body"):
+            shloka = mantra.css("font::text").getall()
 
         # remove new-line chars from string
         shloka = list(map(lambda x: x.replace("\n", ""), shloka))
         # remove empty strings
         shloka = list(filter(lambda x: x != "", shloka))
 
-        yield {
-            'title': response.css('h1.page-title::text').get(),
-            'mantra': shloka
-        }
+        yield {"title": response.css("h1.page-title::text").get(), "mantra": shloka}
 
         try:
-            next_page = response.css(
-                'p.navigation_block a::attr(href)')[2].get()
-        except IndexError as e:
-            next_page = response.css(
-                'p.navigation_block a::attr(href)')[1].get()
+            next_page = response.css("p.navigation_block a::attr(href)")[2].get()
+        except IndexError:
+            next_page = response.css("p.navigation_block a::attr(href)")[1].get()
 
-        pages = re.findall(r'\d+', next_page)
+        pages = re.findall(r"\d+", next_page)
         flag = self.check_start_page(pages)
 
-        if(not flag):
+        if not flag:
             yield response.follow(next_page, callback=self.parse_mantra)
 
     def check_start_page(self, page):
@@ -53,15 +48,14 @@ class MantraSpider(scrapy.Spider):
                 chk = False
                 break
 
-        if (chk == True):
+        if chk is True:
             return True
         else:
             return False
 
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'csv',
-    'FEED_URI': '../../data/raw/gitas/gitas.csv'
-})
+process = CrawlerProcess(
+    settings={"FEED_FORMAT": "csv", "FEED_URI": "../../data/raw/gitas/gitas.csv"}
+)
 process.crawl(MantraSpider)
 process.start()
